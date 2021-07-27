821e6d8b440a161f129a683703c18f4e8ce48190
git config --global user.name Suhas <2612srk@gmail.com>
2021-07-27 23:09:12 +0530

    Initial commit

---
+++ src/main/java/org/example/controller/EShopController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EShopController {
+
+
+    @GetMapping("/checkout")
+    public String checkout() {
+        return "You have successfully checked out your shopping cart.";
+    }
+}
